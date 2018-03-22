05ad7586e914fa92505362930f36d169a08a5e61
Gregor Adams <greg@pixelass.com>
2018-03-23 00:14:32 +0100

    chore: remove travis and shell scripts
    circle-ci replaces travis, lerna/yarn replaces the need for scripts
    
    closes #20

--- script/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-for plugin in $(ls -1 -d esdoc2-*)
-do
-  echo $plugin
-  (cd $plugin && npm install)
-  echo ========================================================================
-done
